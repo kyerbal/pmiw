08510c69799ffe4207833573faaa9e5565f853f5
kyerbal <103000850+kyerbal@users.noreply.github.com>
2024-10-19 17:49:51 -0300

    Add files via upload

--- tpfinalparte1.js
+++ tpfinalparte1.js
@@ -1,46 +1,63 @@
-//Leandro Isasi - Facundo Cristobal
+//Leandro Isasi - Facundo Cristóbal
 //Comisión 5
-//link
+//https://youtu.be/jBCanXGr8is 
 
-let pantallas = []; 
-let pantallaActual = 0; 
-let mySound
+let pantallas = [];
+let pantallaActual = 0;
+let mySound;
+let textos = [];
 
 function preload() {
-  // Cargamos todas las imágenes de las pantallas
-  pantallas[0] = loadImage('data/pantalla0.jpg'); 
-  pantallas[1] = loadImage('data/pantalla1.jpg'); 
-  pantallas[2] = loadImage('data/pantalla2.jpg'); 
-  pantallas[3] = loadImage('data/pantalla2.0.jpg'); 
-  pantallas[4] = loadImage('data/pantalla3.jpg'); 
-  pantallas[5] = loadImage('data/pantalla4.jpg'); 
-  pantallas[6] = loadImage('data/pantalla4.0.jpg'); 
-  pantallas[7] = loadImage('data/pantalla5.jpg'); 
-  pantallas[8] = loadImage('data/pantalla6.jpg'); 
-  pantallas[9] = loadImage('data/pantalla7.jpg'); 
-  pantallas[10] = loadImage('data/pantalla8.jpg'); 
-  pantallas[11] = loadImage('data/pantalla9.jpg'); 
-  pantallas[12] = loadImage('data/pantalla10.jpg'); 
-  pantallas[13] = loadImage('data/pantalla13.jpg'); 
-  pantallas[14] = loadImage('data/pantalla14.jpg'); 
-  pantallas[15] = loadImage('data/creditos.jpg'); 
-  mySound = loadSound ('data/cancion.mp3');
+  for (let i = 0; i < 16; i++) {
+    if (i === 3) {
+      pantallas[i] = loadImage('data/pantalla3.jpg');
+    } else if (i === 6) {
+      pantallas[i] = loadImage('data/pantalla6.jpg');
+    } else if (i === 15) {
+      pantallas[i] = loadImage('data/creditos.jpg');
+    } else {
+      pantallas[i] = loadImage('data/pantalla' + i + '.jpg');
+    }
+  }
+  
+  mySound = loadSound('data/cancion.mp3');
+  textos = loadStrings('data/textos.txt');
 }
 
 function setup() {
-  createCanvas(640, 480); 
+  createCanvas(640, 480);
 }
 
 function draw() {
   background(200);
   image(pantallas[pantallaActual], 0, 0, 640, 480);
+  let textoActual = textos[pantallaActual];
+
+  // Definir el tamaño y la posición del rectángulo
+  let textoAnchoMaximo = 600; // Ancho máximo para el texto
+  let textoAlto = 60; // Altura del rectángulo
+
+  let rectX = 20; // Posición X del rectángulo
+  let rectY = 45; // Posición Y del rectángulo
+
+  // rectángulo de los textos
+  fill(0);
+  rect(rectX, rectY, textoAnchoMaximo + 10, textoAlto + 1); // +1 para un borde adicional
+
+  // configuracion del texto
+  fill(255);
+  textSize(16);
+  textAlign(CENTER); 
+
+  // Dibujar el texto dentro del rect
+  text(textoActual, rectX + 5, rectY + 5, textoAnchoMaximo, textoAlto);
 
   if (pantallaActual === 0) {
-    mostrarBotonesInicio(); 
-  } else if (pantallaActual === 3) {
-    mostrarBotonReiniciar();
+    mostrarBotonesInicio();
   } else if (pantallaActual === 2) {
     mostrarBotonesSiNo();
+  } else if (pantallaActual === 3 || pantallaActual === 12 || pantallaActual === 14 || pantallaActual === 15) {
+    mostrarBotonReiniciar();
   } else if (pantallaActual === 4) {
     mostrarBotonesRojoBlanco();
   } else if (pantallaActual === 5) {
@@ -49,11 +66,7 @@ function draw() {
     mostrarBotonesCamino();
   } else if (pantallaActual === 10) {
     mostrarBotonesEnfrentarteYEntregarte();
-  } else if (pantallaActual === 12 || pantallaActual === 14) {
-    mostrarBotonReiniciar();
-  } else if (pantallaActual === 15){
-    mostrarBotonReiniciar();
-  } else {
-    mostrarBotonAvanzar(); 
+  } else if (pantallaActual === 1 || pantallaActual === 6 || pantallaActual === 7 || pantallaActual === 9 || pantallaActual === 11 || pantallaActual === 13) {
+    mostrarBotonAvanzar();
   }
 }
